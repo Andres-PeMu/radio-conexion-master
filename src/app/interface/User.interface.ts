403b26bc7e30c1bebd932ee1a6a4d82dc463419c
Andres-PeMu <andres.pena.m15@gmail.com>
2022-12-19 00:00:50 -0500

    iniciando interface, iniciando comentarios de card

---
+++ src/app/interface/User.interface.ts
@@ -0,0 +1,10 @@
+export default interface User {
+  id?: string;
+  photoURL: string;
+  name: string;
+  lastName: string;
+  mail: string;
+  password: string;
+  birthday: Date;
+  gender: string;
+}
